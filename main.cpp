d53ffe083fb7051bfef07c335317d652f5fce6e5
MiroshnikovAM <kriogenika@yandex.ru>
2020-04-20 13:51:48 +0300

    code: function decomposition

--- main.cpp
+++ main.cpp
@@ -4,23 +4,44 @@
 using namespace std;
 
 vector<double> input_numbers(size_t count);
+void find_minmax(const vector<double> & numbers, double& min, double& max);
+vector<size_t> make_histogram(const vector<double> & numbers, size_t bin_count, double min, double max);
+void show_histogram_text(vector<size_t>bins);
 
 int main() {
-    // Ввод данных
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
 
     cerr << "Enter numbers: ";
-    vector<double> numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(number_count);
 
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
 
-    // Обработка данных
-    double min = numbers[0];
-    double max = numbers[0];
+    double min, max;
+    find_minmax(numbers, min, max);
+
+    const auto bins = make_histogram(numbers, bin_count, min, max);
+
+    show_histogram_text(bins);
+
+
+    return 0;
+}
+
+vector<double> input_numbers(size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        cin >> result[i];
+    }
+    return result;
+}
+
+void find_minmax(const vector<double> & numbers, double& min, double& max) {
+    min = numbers[0];
+    max = numbers[0];
     for (double number : numbers) {
         if (number < min) {
             min = number;
@@ -29,7 +50,9 @@ int main() {
             max = number;
         }
     }
+}
 
+vector<size_t> make_histogram(const vector<double> & numbers, size_t bin_count, double min, double max) {
     vector<size_t> bins(bin_count);
     for (double number : numbers) {
         size_t bin = (size_t)((number - min) / (max - min) * bin_count);
@@ -38,8 +61,10 @@ int main() {
         }
         bins[bin]++;
     }
+    return(bins);
+}
 
-    // Вывод данных
+void show_histogram_text(vector<size_t>bins) {
     const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
@@ -71,14 +96,5 @@ int main() {
         }
         cout << '\n';
     }
-
-    return 0;
 }
 
-vector<double> input_numbers(size_t count) {
-    vector<double> result(count);
-    for (size_t i = 0; i < count; i++) {
-        cin >> result[i];
-    }
-    return result;
-}
