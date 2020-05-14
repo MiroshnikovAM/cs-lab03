1c83f216f130d5c43896a3d9730b0995177dabc1
MiroshnikovAM <kriogenika@yandex.ru>
2020-05-15 01:21:22 +0300

    code: read_input applied\

--- main.cpp
+++ main.cpp
@@ -5,35 +5,26 @@
 
 using namespace std;
 
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
+
 vector<double> input_numbers(istream& in, size_t count);
+Input read_input(istream& in);
 
 int main() {
 
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
-
-    if (number_count == 0) {
-        cerr << "ERROR: Empty vector";
-    } else {
-
-    cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(cin, number_count);
-
-    size_t bin_count;
-    cerr << "Enter column count: ";
-    cin >> bin_count;
+    const auto input = read_input(cin);
 
     double min, max;
-    find_minmax(numbers, min, max);
+    find_minmax(input.numbers, min, max);
 
-    const auto bins = make_histogram(numbers, bin_count, min, max);
+    const auto bins = make_histogram(input.numbers, input.bin_count, min, max);
     cout << show_histogram_svg(bins);
 
-    }
     return 0;
 }
-
 
 vector<double> input_numbers(istream& in, size_t count) {
     vector<double> result(count);
@@ -43,3 +34,19 @@ vector<double> input_numbers(istream& in, size_t count) {
     return result;
 }
 
+Input read_input(istream& in) {
+
+    Input data;
+
+    cerr << "Enter number count: ";
+    size_t number_count;
+    in >> number_count; // cin ?
+
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
+
+    cerr << "Enter column count: ";
+    in >> data.bin_count; // cin ?
+
+    return data;
+}
