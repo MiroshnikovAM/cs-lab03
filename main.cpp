8fe3570e1e4662e288c01f38646596ed175e41d4
MiroshnikovAM <kriogenika@yandex.ru>
2020-05-15 01:41:18 +0300

    code: now u can choose to use prompts

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 
 vector<double> input_numbers(istream& in, size_t count);
-Input read_input(istream& in);
+Input read_input(istream& in, bool prompt);
 
 int main() {
-    const auto input = read_input(cin);
+    const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
     cout << show_histogram_svg(bins);
     return 0;
@@ -23,18 +23,24 @@ vector<double> input_numbers(istream& in, size_t count) {
     return result;
 }
 
-Input read_input(istream& in) {
+Input read_input(istream& in, bool prompt) {
 
     Input data;
 
-    cerr << "Enter number count: ";
+    if (prompt) {
+        cerr << "Enter number count: ";
+    }
     size_t number_count;
     in >> number_count; // cin ?
 
-    cerr << "Enter numbers: ";
+    if (prompt) {
+        cerr << "Enter numbers: ";
+    }
     data.numbers = input_numbers(in, number_count);
 
-    cerr << "Enter column count: ";
+    if (prompt) {
+        cerr << "Enter column count: ";
+    }
     in >> data.bin_count; // cin ?
 
     return data;
