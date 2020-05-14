50accb66ddeb4c9371ce16f0a0ca39b00f17a834
MiroshnikovAM <kriogenika@yandex.ru>
2020-05-15 01:03:57 +0300

    code: whatever flow

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-vector<double> input_numbers(size_t count);
+vector<double> input_numbers(istream& in, size_t count);
 
 int main() {
 
@@ -18,7 +18,7 @@ int main() {
     } else {
 
     cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(cin, number_count);
 
     size_t bin_count;
     cerr << "Enter column count: ";
@@ -34,10 +34,11 @@ int main() {
     return 0;
 }
 
-vector<double> input_numbers(size_t count) {
+
+vector<double> input_numbers(istream& in, size_t count) {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++) {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
