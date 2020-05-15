dc5378b67acbf282783e0ababeddb577d0b8fe0e
MiroshnikovAM <kriogenika@yandex.ru>
2020-05-15 17:17:28 +0300

    code: download func release

--- main.cpp
+++ main.cpp
@@ -59,7 +59,10 @@ Input read_input(istream& in, bool prompt) {
 
 size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
     // TODO: дописывать данные к буферу.
-    return 0;
+    size_t data_size = item_size * item_count;
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    (*buffer).write(reinterpret_cast<const char*>(items), data_size);
+    return data_size;
 }
 
 Input download(const string& address) {
@@ -70,6 +73,8 @@ Input download(const string& address) {
     if(curl) {
         CURLcode res;
         curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
         res = curl_easy_perform(curl);
         if (res) {
             cerr << curl_easy_strerror(res) << endl;
