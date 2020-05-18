f6e14476121bf1089e2a00aeca49e331f6e145ec
MiroshnikovAM <kriogenika@yandex.ru>
2020-05-18 13:41:06 +0300

    code: printing win version

--- main.cpp
+++ main.cpp
@@ -3,18 +3,22 @@
 #include "histogram.h"
 #include "svg_module.h"
 #include <cstdio>
+#include <windows.h>
 
 using namespace std;
 
 vector<double> input_numbers(size_t count);
 
 int main() {
-
+    /*
     const char* name = "Commander Shepard";
     int year = 2154;
     printf("%s was born in %d.\n", name, year);
     // Commander Shepard was born in 2154.
     printf("n = %08x\n", 0x1234567); // 01234567
+    */
+    printf("Windows (decimal) version is %u.\n", GetVersion());
+    printf("Windows (16x) version is %x.\n", GetVersion());
     return 0;
 
     size_t number_count;
